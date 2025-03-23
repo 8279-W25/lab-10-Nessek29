af1b15cbfe2b0cef1b193214a7497e0e00b8c0b5
Simbil Nessek <ness0029@algonquinlive.com>
2025-03-23 10:51:45 -0400

    Created a function to convert the user input to lower case and clean it by removing invalid characters

--- lab10.py
+++ lab10.py
@@ -13,3 +13,14 @@ morse_dictionnary={
 # Prompt the user to enter a sentence 
 phrase = input('Enter a sentence: ')
 
+# clean input by converting lower case and removing invalid characters to match the dictionnary keys.
+def cleaned_input(phrase):
+    phrase= phrase.lower()
+    new_phrase= ''
+    # loop through each characters in the phrase and filter valid ones
+    for char in phrase:
+        if char in morse_dictionnary:
+            # if so, add valid character to new_phrase
+            new_phrase += char
+
+    return new_phrase
